867eb3e50b1cdc0be42590b0622d48ae1f5223eb
Chapman <Chapman@Apextion.com>
2016-05-06 12:27:15 -0400

    fix Envar changed to match production

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
-// Connection strigh is set to MONGOLAB_URI if availible or default to localhost
-var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/slackbot';
+// Connection strigh is set to MONGODB_URI if availible or default to localhost
+var uristring = process.env.MONGODB_URI || 'mongodb://localhost/slackbot';
 
 mongoose.connect(uristring, function(err, res) {
 	if (err) {
